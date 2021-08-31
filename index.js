95feb45218fe221bca524dde0877f9bcead775de
Job Dorado <jobdorado15@gmail.com>
2021-08-30 23:33:22 -0500

    Actividad 7. Router, Morgan y MySQL

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
-
+const morgan =require('morgan');
 const express = require('express');
 const app = express();
-const { pokemon } = require('./pokedex.json');
+const pokemon = require('./routes/pokemon')
 
+app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 app.get("/", (rec, res, next) => {
     return res.status(200).send("Bienvenido al Pokedex");
 });
 
-app.post("/pokemon", (req, res, next) => {
-    return res.status(200).send(req.body);
-});
-
-app.get("/pokemon", (req, res, next) => {
-    return res.status(200).send(pokemon);
-});
-
-app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
-    const id = req.params.id - 1;
-    if(id >= 0 && id <= 150){
-        return res.status(200).send(pokemon[req.params.id -1]);
-    }
-    return res.status(404).send("Pokemon no encontrado");
-});
-
-app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) => {
-    const name = req.params.name;
-    const pk = pokemon.filter((p) => {
-        return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
-    });
-
-    //This is a "if" Condition ? true : false
-    (pk.length > 0 ) ? res.status(200).send(pk) : res.status(200).send(pk);
-});
+app.use("/pokemon", pokemon);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");    
