b2c20c51a084a4016a500a16068a34c3a6bbca5d
Job Dorado <jobdorado15@gmail.com>
2021-10-06 01:06:43 -0500

    Actividad 15. Conectando con la interfaz de usuario (2)

--- routes/user.js
+++ routes/user.js
@@ -35,11 +35,11 @@ user.post("/login", async (req, res, next) =>{
             return res.status(200).json({ code: 200, message: token});
         }
         else{
-            return res.status(200).json({ code: 200, message: "Usuario y/o contrasena incorrectos"});
+            return res.status(200).json({ code: 401, message: "Usuario y/o contrasena incorrectos"});
         }
         
     }
-    return res.status(401).json({ code: 401, message: "Canmpos incompletos"});
+    return res.status(500).json({ code: 500, message: "Canmpos incompletos"});
 
     
 });
